30a24c4b5b1ae1e04e5fd4c2bec6a461fbcc71c0
unknown <lamzonelizer1@gmail.com>
2022-11-05 00:02:58 +0800

    added stuff in landing page

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/*
+Carousel
+*/
+console.log("JS Connected");
+$('#project-carousel').on('slide.bs.carousel', function(e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var idx = $(e.relatedTarget).index(); 
+    // console.log("previous element: ");
+    // console.log($(e.relatedTarget).prev());
+    // console.log("current element: ");
+    // console.log(e);
+    // console.log(idx);
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item').length; // 8
+    if (idx >= totalItems - (itemsPerSlide - 1)) { // 7 >= 8 - 4 = 4
+        var it = itemsPerSlide - (totalItems - idx); // 5-(8-1) = 2
+        for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            } else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
